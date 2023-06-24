5ae1ed8eb74eb20e1c3c362b6b698b238422b3c6
nick2918 <35611616+nick2918@users.noreply.github.com>
2023-06-24 00:16:00 +0000

    landing page lista

---
+++ src/js/component/cards.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+const Cards = () => {
+    return (
+        <>
+           <div className="card-group">
+  <div className="card">
+    <img src="https://tecnovortex.com/wp-content/uploads/2019/04/wallpaper-engine.jpg" className="card-img-top" alt="..."/>
+    <div className="card-body">
+      <h5 className="card-title">Card title</h5>
+      <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    </div>
+    <div className="card-footer">
+      <small className="text-body-secondary">Last updated 3 mins ago</small>
+    </div>
+  </div>
+  <div className="card">
+    <img src="https://tecnovortex.com/wp-content/uploads/2019/04/wallpaper-engine.jpg" className="card-img-top" alt="..."/>
+    <div className="card-body">
+      <h5 className="card-title">Card title</h5>
+      <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+    </div>
+    <div className="card-footer">
+      <small className="text-body-secondary">Last updated 3 mins ago</small>
+    </div>
+  </div>
+  <div className="card">
+    <img src="https://tecnovortex.com/wp-content/uploads/2019/04/wallpaper-engine.jpg" className="card-img-top" alt="..."/>
+    <div className="card-body">
+      <h5 className="card-title">Card title</h5>
+      <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+    </div>
+    <div className="card-footer">
+      <small className="text-body-secondary">Last updated 3 mins ago</small>
+    </div>
+  </div>
+</div>
+        </>
+    )
+}
+
+export default Cards
---
+++ src/js/component/footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <>
+           <div className="card-footer bg-dark" style={{color: "white"}}>
+            © Copyright 2023 - Landing page
+        </div> 
+        </>
+    )
+}
+
+export default Footer
